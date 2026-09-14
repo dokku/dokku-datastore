c8f99f5c0799cf08c10c47e3bffb6edd4c0ce515
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2026-09-14 14:29:44 -0400

    feat: implement the clone command
    The new service is pinned to whatever image the source is running, so copied
    data is never handed to a different version than it came from. The dump is
    staged on disk rather than piped, so a failed export cannot feed a truncated
    stream to the import.

---
+++ internal/clone.go
@@ -0,0 +1,125 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/dokku/dokku-datastore/internal/datastores"
+)
+
+// CloneServiceInput is the input for the CloneService function
+type CloneServiceInput struct {
+	// ConfigOptions are extra arguments passed to the container create command
+	ConfigOptions string
+
+	// CustomEnv is the environment the new service is started with
+	CustomEnv string
+
+	// Datastore is the datastore both services belong to
+	Datastore datastores.Datastore
+
+	// Logger reports progress
+	Logger Ui
+
+	// NewServiceName is the name of the service to create
+	NewServiceName string
+
+	// Password overrides the generated service password
+	Password string
+
+	// ServiceName is the name of the service to copy from
+	ServiceName string
+
+	// ShmSize overrides the shared memory size of the new container
+	ShmSize string
+
+	// Memory limits the memory of the new container
+	Memory int
+
+	// InitialNetwork is the network the new container is attached to on create
+	InitialNetwork string
+
+	// PostCreateNetworks are attached after the new container is created
+	PostCreateNetworks []string
+
+	// PostStartNetworks are attached after the new container is started
+	PostStartNetworks []string
+}
+
+// CloneService creates a new service on the same image as an existing one and
+// copies the data across
+func CloneService(ctx context.Context, input CloneServiceInput) error {
+	// the clone runs on whatever image the source service is on, so that the
+	// copied data is never handed to a different version than it came from
+	sourceImage := datastores.Version(ctx, datastores.VersionInput{
+		Datastore:   input.Datastore,
+		ServiceName: input.ServiceName,
+	})
+	image, imageVersion, found := strings.Cut(sourceImage, ":")
+	if !found {
+		return fmt.Errorf("unable to determine the image %s is running", input.ServiceName)
+	}
+
+	input.Logger.Header2(fmt.Sprintf("Cloning %s to %s @ %s", input.ServiceName, input.NewServiceName, sourceImage)) //nolint:errcheck
+
+	if err := CreateService(ctx, CreateServiceInput{
+		ConfigOptions:      input.ConfigOptions,
+		CustomEnv:          input.CustomEnv,
+		Datastore:          input.Datastore,
+		Image:              image,
+		ImageVersion:       imageVersion,
+		InitialNetwork:     input.InitialNetwork,
+		Memory:             input.Memory,
+		Password:           input.Password,
+		PostCreateNetworks: input.PostCreateNetworks,
+		PostStartNetworks:  input.PostStartNetworks,
+		ServiceName:        input.NewServiceName,
+		ShmSize:            input.ShmSize,
+	}); err != nil {
+		return err
+	}
+
+	input.Logger.Info(fmt.Sprintf("Copying data from %s to %s", input.ServiceName, input.NewServiceName))
+
+	// the dump is staged on disk rather than piped, so that a failed export does
+	// not hand a truncated stream to the import
+	dumpFile, err := os.CreateTemp("", "dokku-datastore-clone")
+	if err != nil {
+		return fmt.Errorf("unable to create a temporary file: %w", err)
+	}
+	defer os.Remove(dumpFile.Name())
+
+	if err := input.Datastore.ExportService(ctx, datastores.ExportServiceInput{
+		Datastore:   input.Datastore,
+		ServiceName: input.ServiceName,
+		Writer:      dumpFile,
+	}); err != nil {
+		dumpFile.Close()
+		return fmt.Errorf("unable to export %s: %w", input.ServiceName, err)
+	}
+
+	if _, err := dumpFile.Seek(0, io.SeekStart); err != nil {
+		dumpFile.Close()
+		return fmt.Errorf("unable to rewind %s: %w", filepath.Base(dumpFile.Name()), err)
+	}
+
+	if err := input.Datastore.ImportService(ctx, datastores.ImportServiceInput{
+		Datastore:   input.Datastore,
+		Reader:      dumpFile,
+		ServiceName: input.NewServiceName,
+	}); err != nil {
+		dumpFile.Close()
+		return fmt.Errorf("unable to import into %s: %w", input.NewServiceName, err)
+	}
+
+	if err := dumpFile.Close(); err != nil {
+		return fmt.Errorf("unable to close the temporary file: %w", err)
+	}
+
+	input.Logger.Header2("Done") //nolint:errcheck
+	return nil
+}
